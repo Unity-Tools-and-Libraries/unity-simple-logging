6def4f090b0fc9239de7e6c1d4221459f2c50edb
Damien Marble <damienmarble@gmail.com>
2024-07-13 12:39:36 -0700

    Simplified logging interface.

--- Assets/Runtime/Scripts/ILogger.cs
+++ Assets/Runtime/Scripts/ILogger.cs
@@ -6,20 +6,7 @@ namespace io.github.thisisnozaku.logging
 {
     public interface ILogger
     {
-        void Log(LogType unityLogType, string logMessage, params string[] logContext);
-        void Log(LogLevel logLevel, string logMessage, params string[] logContext);
-        void Log(LogLevel logLevel, Func<string> messageGenerator, params string[] logContext);
-        void Fatal(string logMessage, params string[] logContext);
-        void Fatal(Func<string> messageGenerator, params string[] logContext);
-        void Error(string logMessage, params string[] logContext);
-        void Error(Func<string> messageGenerator, params string[] logContext);
-        void Warn(string logMessage, params string[] logContext);
-        void Warn(Func<string> messageGenerator, params string[] logContext);
-        void Info(string logMessage, params string[] logContext);
-        void Info(Func<string> messageGenerator, params string[] logContext);
-        void Debug(string logMessage, params string[] logContext);
-        void Debug(Func<string> messageGenerator, params string[] logContext);
-        void Trace(string logMessage, params string[] logContext);
-        void Trace(Func<string> messageGenerator, params string[] logContext);
+        void Log(LogLevel unityLogType, string logMessage, params string[] logContext);
+        void Log(LogLevel unityLogType, Func<string> logMessage, params string[] logContext);
     }
 }
